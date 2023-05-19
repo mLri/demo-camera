25d4489e5673c1963c1f4b6db99fc595ef8a89c8
Tosaporn Maphasuk <tosaporn.m@g-able.com>
2023-05-19 14:30:35 +0700

    Init project

---
+++ test.js
@@ -0,0 +1,73 @@
+const a = {
+  tree_structor: [
+    {
+      code: '',
+      name: '',
+      canAdd: true,
+      canDel: false,
+      isShowBorderleft: false,
+      isShowLeftLine: false,
+      campuses: [
+        {
+          name: '',
+          canAdd: true,
+          canDel: false,
+          isShowBorderleft: false,
+          isShowLeftLine: false,
+          facultyGroups: [
+            {
+              name: '',
+              canAdd: true,
+              canDel: false,
+              isShowBorderleft: false,
+              isShowLeftLine: false,
+              faculties: [
+                {
+                  name: '',
+                  canAdd: true,
+                  canDel: false,
+                  isShowBorderleft: false,
+                  isShowLeftLine: false,
+                  departments: [
+                    {
+                      name: '',
+                      canAdd: true,
+                      canDel: false,
+                      isShowBorderleft: false,
+                      isShowLeftLine: false,
+                      branchs: [
+                        {
+                          name: '',
+                          canAdd: true,
+                          canDel: false,
+                          isShowBorderleft: false,
+                          isShowLeftLine: false,
+                          subBrachs: [
+                            {
+                              name: '',
+                              canAdd: true,
+                              canDel: false,
+                              isShowBorderleft: false,
+                              isShowLeftLine: false,
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  data: {
+    faculties: [
+      {
+        
+      }
+    ]
+  }
+}
